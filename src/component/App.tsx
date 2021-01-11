0b4068ac6e8ff3bf9445a718da5a53f43ced49a3
kimts2627 <kimtstststs@gmail.com>
2021-01-11 17:16:27 +0900

    Edit SCSS settings

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import '../style/App.scss';
 import React from 'react';
 
 function App() {
---
+++ src/modules/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  //counter
+});
+
+export default rootReducer;
