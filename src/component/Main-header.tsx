8006c24e6d8077b3c28ce8fc727b32e5b27f866a
kimts2627 <kimtstststs@gmail.com>
2021-01-13 00:09:36 +0900

    Add header, service logo

--- src/component/Main-header.tsx
+++ src/component/Main-header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../img/logo.png';
 
 const MainHeader = () => {
 
 
   return(
     <header className='header'>
-
+      <img src={logo} alt='' className='logo'/>
     </header>
   );
 }
